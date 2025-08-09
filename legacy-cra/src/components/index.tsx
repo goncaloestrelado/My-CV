9aef4f866181e8fd7da16b30f3d7a193b7ed8c80
goncaloestrelado <goncalo.estrelado@hotmail.com>
2025-08-09 17:59:38 +0100

    deleted legacy-cra

--- legacy-cra/src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { Header } from "./Header";
-export { Layout } from "./Layout";
-export { Footer } from "./Footer";
-export { Card } from "./Card";
--- legacy-cra/src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Home } from "./Home";
-export { Projects } from "./Projects";
-export { NotFound } from "./NotFound";
-export { About } from "./About";
-export { Contact } from "./Contact";
